559ae06f81677602390f562b1827ee5b1fa37d5e
mariaantipina <91631167+mariaantipina@users.noreply.github.com>
2021-09-29 22:22:37 +0300

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin » a » b;
+	setlocale(LC_ALL, "Rus");
+	switch (a) {
+	case 1:
+		cout « 365 - (31 - (31 - b)) « endl; break;
+	case 3:
+		cout « 365 - ((28 + (2 * 31)) - (31 - b)) « endl; break;
+	case 5:
+		cout « 365 - ((28 + (3 * 31) + 30) - (31 - b)) « endl; break;
+	case 7:
+		cout « 365 - ((28 + (4 * 31) + (30 * 2)) - (31 - b)) « endl; break;
+	case 8:
+		cout « 365 - ((28 + (5 * 31) + (30 * 2)) - (31 - b)) « endl; break;
+	case 10:
+		cout « 365 - ((28 + (6 * 31) + (30 * 3)) - (31 - b)) « endl; break;
+	case 12:
+		cout « 365 - ((28 + (7 * 31) + (30 * 4)) - (31 - b)) « endl; break;
+	case 4:
+		cout « 365 - (28 + (2 * 31) + 30 - (30 - b)) « endl; break;
+	case 6:
+		cout « 365 - (28 + (3 * 31) + (2 * 30) - (30 - b)) « endl; break;
+	case 9:
+		cout « 365 - (28 + (5 * 31) + (3 * 30) - (30 - b)) « endl; break;
+	case 11:
+		cout « 365 - (28 + (6 * 31) + (4 * 30) - (30 - b)) « endl; break;
+	case 2:
+		if (b < 29) {
+			cout « 365 - (28 + 31 - (28 - b)) « endl; break;
+		}
+		else {
+			cout « "Îøèáêà" « endl; break;
+		}
+	default:
+		cout « "Îøèáêà" « endl; break;
+	}
+
+	return 0;
+}

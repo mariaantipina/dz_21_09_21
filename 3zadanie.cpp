7bda31c7619119e86f8d5f667c3666733277a19b
mariaantipina <91631167+mariaantipina@users.noreply.github.com>
2021-10-14 23:02:11 +0300

    Add files via upload

---
+++ 3zadanie.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int summadel(int n) {
+	int sum = 0;
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	for (int i = 1; i <= 10000; i++) {
+		int m = summadel(i);
+		if (m <= i)
+			continue;
+		if (i == summadel(m) && m <= 10000) {
+			cout << "Пара дружественных чисел: {" << i << ";" << m << "}" << endl;
+		}
+	}
+}

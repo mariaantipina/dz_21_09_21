4b0db05482cb9a225a3e22412e90f083bb1e3986
mariaantipina <91631167+mariaantipina@users.noreply.github.com>
2021-10-14 21:59:55 +0300

    Add files via upload

---
+++ 2zadanie.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int N, c;
+	cin >> N;
+	int a = 1, b = 1;
+	for (c = 1; c <= N; c++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	cout << "число Фибоначчи = " << c << endl;
+	return 0;
+}
